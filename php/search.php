963644fb4f27db1f565bc3b2749af4ea49589974
DESKTOP-79RM8UR\andre <andres_g_1000@hotmail.com>
2017-08-25 12:20:16 -0400

    Enter new data

---
+++ php/search.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+$mysql_userB = 'root';
+$mysql_passB = '';
+$mysql_host = '127.0.0.1';
+$mysql_db = 'magic';
+$conn = mysqli_connect($mysql_host,$mysql_userB,$mysql_passB,$mysql_db);
+
+if(isset($_GET['name'])){
+
+	$name = mysqli_real_escape_string($conn,$_GET['name']);
+
+	$sql = "SELECT artist FROM cards WHERE (name LIKE '%$name%')";
+		if($sql_run = mysqli_query($conn,$sql)){
+
+            while($row = $sql_run->fetch_assoc()) {
+				echo $row['artist']."<br>";
+			}
+		}
+}
+
+?>
