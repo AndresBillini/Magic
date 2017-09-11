70a4a1b6a710b59423ced05ca6331cff737dd485
Andrés Billini <andres_g_1000@hotmail.com>
2017-09-11 12:31:41 -0400

    multiple search

--- php/db-local.php
+++ php/db-local.php
@@ -3,7 +3,7 @@
 $mysql_userB = 'root';
 $mysql_passB = '';
 $mysql_host = '127.0.0.1';
-$mysql_db = 'magic';
+$mysql_db = 'fs_magic';
 $conn = mysqli_connect($mysql_host,$mysql_userB,$mysql_passB,$mysql_db);
 
 ?>
